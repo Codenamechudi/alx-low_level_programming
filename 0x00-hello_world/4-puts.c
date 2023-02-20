4c2b6f45e936ad755b0e44bce490371426f5492c
Codenamechudi <ilohchukwudi@gmail.com>
2023-02-20 14:27:49 +0000

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
-
+/**
+ * main - just a printe
+ * Return: it shoud be 0
+ */
 int main(void)
-
 	{
 		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
