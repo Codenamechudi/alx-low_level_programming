53906c409f82fbe24fa67c3e2235960ebc6e786a
Codenamechudi <ilohchukwudi@gmail.com>
2023-03-23 22:47:17 +0000

    projects on variadic functions in c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,10 +12,15 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
+
 	char *str;
+
 	unsigned int index;
+
 	va_start(strings, n);
+
 	for (index = 0; index < n; index++)
+
 	{
 		str = va_arg(strings, char *);
 		if (str == NULL)
